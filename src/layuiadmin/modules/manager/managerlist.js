6cec8fe3c105feaf7bb7c9b79b00424c1eec22dc
shixia <1498047434@qq.com>
2021-06-06 23:16:33 +0800

    v2.5.0 管理员学生管理完成

--- src/layuiadmin/modules/manager/managerlist.js
+++ src/layuiadmin/modules/manager/managerlist.js
@@ -305,7 +305,91 @@
     }
   });
 
+  //学生管理
+  table.render({
+    elem: '#LAY-app-student-list'
+    ,url: layui.setter.reqUrl + '/student/listByPageForManager' //模拟接口
+    ,cols: [[
+      {type: 'checkbox', fixed: 'left'}
+      ,{field: 'id', width: 200, title: '学生ID', sort: true}
+      ,{field: 'username', title: '学生账号（学号）', minWidth: 300}
+      ,{field: 'name', title: '学生姓名', minWidth: 300}
+      ,{field: 'clazzName', title: '学生班级', minWidth: 300}
+      ,{field: 'state', title: '状态', templet: '#buttonTpl', minWidth: 120, align: 'center'}
+      ,{title: '操作', width: 250, align: 'center', fixed: 'right', toolbar: '#table-student-list'}
+    ]]
+    ,page: true
+    ,limit: 10
+    ,limits: [10, 15, 20, 25, 30]
+    ,text: '对不起，加载出现异常！'
+  });
+  
+  //教师管理监听工具条
+  table.on('tool(LAY-app-student-list)', function(obj){
+    var data = obj.data;
+    
+    if(obj.event === 'del'){
+      console.log(data)
+      console.log(JSON.stringify(data))
+      layer.confirm('确定删除此教师账号吗？', function(index){
+        //请求服务器删除
+        admin.req({
+          url: layui.setter.reqUrl + '/student/delForManager' //实际使用请改成服务端真实接口
+          ,data: "[" + JSON.stringify(data) + "]"
+          ,type: 'post'
+          ,contentType:'application/json'
+          ,done: function(res){
 
+            //从表格中删除
+            obj.del();
+            layer.msg('已删除');
+          }
+        });
+
+        // //从表格中删除
+        // obj.del();
+
+        layer.close(index);
+      });
+    } else if(obj.event === 'edit'){
+      json = JSON.stringify(data)
+      layer.open({
+        type: 2
+        ,title: '编辑教师'
+        ,content: '../../../views/manager/student/listform.html?id='+ data.id
+        ,maxmin: true
+        ,area: ['550px', '550px']
+        ,btn: ['确定', '取消']
+        ,yes: function(index, layero){
+          var iframeWindow = window['layui-layer-iframe'+ index]
+          ,submit = layero.find('iframe').contents().find("#layuiadmin-app-form-edit");
+          //监听提交
+          iframeWindow.layui.form.on('submit(layuiadmin-app-form-edit)', function(data){
+            var field = data.field; //获取提交的字段
+            
+            //提交 Ajax 成功后，静态更新表格中的数据
+            //$.ajax({});请求服务器删除
+            if(field.state == null){
+              field.state = 0;
+            }
+            admin.req({
+              url: layui.setter.reqUrl + '/student/updateForManager' //实际使用请改成服务端真实接口
+              ,data: field
+              ,type: 'post'
+              ,done: function(res){
+                layui.table.reload('LAY-app-student-list'); //重载表格
+                layer.msg('已更新');
+              }
+            });        
+            form.render();
+            layer.close(index); //关闭弹层
+          });  
+          
+          submit.trigger('click');
+        }
+      });
+    }
+  });
 
 
 
